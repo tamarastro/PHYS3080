153b717cc4c1dc948dfe8b810a0a82f2ea113653
Samuel Hinton <samuelreay@gmail.com>
2017-09-14 15:06:04 +1000

    Slight changes

--- expansion.py
+++ expansion.py
@@ -1,91 +1,66 @@
 # ----------Import necessary packages----------
 import numpy as np
-import matplotlib, matplotlib.pyplot as plt
+import matplotlib
+import matplotlib.pyplot as plt
 from scipy.integrate import cumtrapz
 
 
-# ----------Define the function we want to integrate----------
+def get_adot_inv(a, omega_matter, omega_lambda):
+    """ Calculate 1/(adot/H0) where adot=da/dt
+    
+    Note this is dimensionless, (it's Friedmann's equation divided by H0).
+    adot should be multiplied by H0 to get units.
 
-# Calculate 1/(adot/H0) where adot=da/dt for the chosen
-# scalefactor (a), matter density (om), and cosmological constant density (ol).
-# Note this is dimensionless, (it's Friedmann's equation divided by H0).
-# adot should be multiplied by H0 to get units.
-
-
-def getadotinv(a,om,ol):
-    ok=1.0-om-ol                         # curvature (ok=Omega_k)
-    adot = np.sqrt(ok + om/a + ol*a*a)
-    return 1./adot
-
-# Create a vectorized version of it that allows an array of a values
-vecGetadotinv = np.vectorize(getadotinv, excluded=['om', 'ol'])
-
-#-------------------
-# INITIALISE ARRAYS
-#-------------------
-
-# Initialise an array of scalefactors
-a_lo    = 1.e-10
-a_hi    = 2
-a_nstep = 1001  # by making an odd number of elements, you hit the centre element exactly
-aarr    = np.linspace(a_lo, a_hi, a_nstep)  # Makes an array from a_lo to a_hi with the number of steps being a_nsteps
-
-# Find which array index corresponds to a=1, i.e. corresponds to today
-iToday = np.argmin(np.abs(aarr - 1.0))
-
-# Initialise values of Omega_M to loop over
-om_lo    = 0.0
-om_hi    = 5
-om_nstep = 6
-oms      = np.linspace(om_lo, om_hi, om_nstep)
-
-# Set a value of Omega_Lambda
-ol=0.7
+    """
+    omega_k = 1.0 - omega_matter - omega_lambda  # curvature (ok=Omega_k)
+    adot = np.sqrt(omega_k + omega_matter / a + omega_lambda * a * a)
+    return 1.0 / adot
 
 
-#--------------------------------------------
-# CALCULATE Conversion factors to get into Gyr
-#--------------------------------------------
+# ---------- Initialise our variables ---------------------------
+a_arr = np.linspace(1e-10, 2, 1001)  # Makes an array of scalefactors. 1001 steps from 1e-10 to 2
+omega_matter_arr = np.linspace(0.0, 5.0, 6)  # Makes an array of matter densities. 6 steps from 0.0 to 5.0
 
-H0kmsmpc = 70                 # Choose a value of H0 in km/s/Mpc
-#H0s = H0kmsmpc * 3.24e-20     # Convert from km/s/Mpc to inverse seconds (3.24e-20 Mpc/km)
-#H0y = H0s* 3.156e16           # Convert from inverse seconds to inverse Giga-years (3.15e16 s/Gyr)
+# Set up other helpful variables
+omega_lambda = 0.7
+ind = np.argmin(np.abs(a_arr - 1.0))  # Find which array index corresponds to a=1 (i.e. today)
+
+# Set up conversion factors
+H0kmsmpc = 70  # Choose a value of H0 in km/s/Mpc
 H0y = H0kmsmpc * 0.001022544  # Convert directly to inverse Giga-years
 
 
-#--------------------------------------------
-# CALCULATE time as a function of scalefactor
-#--------------------------------------------
-# To calculate time we need to integrate the function 1/(da/dt), with respect to a
-lookback = []
-for om in oms:
-    adotinvs = vecGetadotinv(aarr, om, ol)       # Get a vector of adotinvs as a function of a
-    ages = cumtrapz(adotinvs, x=aarr, initial=0) # To a trapezoidal integral over that array.
-    lookback.append((ages - ages[iToday])/H0y)   # Shift the arrays by subtracting the current age of the universe
-                                                 # and convert to billions of years by dividing by H0y.
+# ---------- Do the math ---------------------------
+# Now for the fun part! For every matter density we want to calculate the lookback time, by integrating 1/adot wrt a
+lookbacks = []
+for om in omega_matter_arr:
+    adotinvs = get_adot_inv(a_arr, om, omega_lambda)  # Get a vector of adotinvs as a function of a
+    ages = cumtrapz(adotinvs, x=a_arr, initial=0)  # To a trapezoidal integral over that array.
+    lookbacks.append((ages - ages[ind]) / H0y)  # Subtracting the current age of the universe and get into gigayears
 
-# Example analytic solutions for testing
-#tom0_analytic = 1.0/H0y/np.sqrt(ol)*np.log(aarr)  # om=0, ol=1.0
-#tom0_analytic2 = 1.0/H0y/np.sqrt(ol) * ( np.log(aarr+np.sqrt(aarr*aarr+(1.0-ol)/ol)) - np.log(1.0+np.sqrt(1.0+(1.0-ol)/ol)) ) # om=0, ol=ol, ok=1-ol
 
-#--------------------------------------------
-# PLOT scalefactor as a function of time
-#--------------------------------------------
-colours = matplotlib.cm.rainbow(np.linspace(0,1,oms.size))
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-for (om, lookback, colour) in zip(oms, lookback, colours):
-    ax.plot(lookback, aarr, label=r"$\Omega_m=%0.2f$"%om, color=colour)
-#ax.plot(tom0_analytic,aarr,'--',color='black') # Example analytic solutions
-#ax.plot(tom0_analytic2,aarr,'--',color='black')
+# ---------- Plot everything ---------------------------
 
-# plt.xlim([-20,20])  # How to change axis limits if you need to
+colours = matplotlib.cm.rainbow(np.linspace(0, 1, omega_matter_arr.size))  # Get us some colours
 
+fig, ax = plt.subplots(nrows=1, ncols=1)
+for (om, lookback, colour) in zip(omega_matter_arr, lookbacks, colours):
+    ax.plot(lookback, a_arr, label=r"$\Omega_m=%0.2f$" % om, color=colour)
+
+# If we want to plot some analytic solutions, uncomment the relevant lines
+# t_om0_analytic = 1.0/H0y/np.sqrt(omega_lambda)*np.log(a_arr)  # om=0, ol=1.0
+# ax.plot(t_om0_analytic, a_arr, '--', color='k', label=r"$\Omega_m=0, \ \Omega_\Lambda=1$")
+# t_om0_analytic2 = 1.0/H0y/np.sqrt(omega_lambda) * (np.log(a_arr+np.sqrt(a_arr*a_arr+(1.0-omega_lambda)/omega_lambda)) - np.log(1.0+np.sqrt(1.0+(1.0-omega_lambda)/omega_lambda)))  # om=0, ol=ol, ok=1-ol
+# ax.plot(t_om0_analytic2, a_arr, '--', color='gray', label=r"Flat $\Omega_m=0, \ \Omega_\Lambda=%0.2f$" % omega_lambda)
+
+ax.set_xlim([-20, 20])  # How to change axis limits if you need to
 ax.axvline(0, color="k", ls=":")
 ax.axhline(1, color="k", ls=":")
-ax.legend(loc="upper left",frameon=False)#,bbox_to_anchor=(0, 0.5))
-plt.figtext(0.88,0.1,"$\Omega_\Lambda=%0.2f$"%ol,ha='right',va='bottom',weight='roman', size='large')
+ax.legend(loc="upper left", frameon=False)
+plt.figtext(0.88, 0.12, r"$\Omega_\Lambda=%0.2f$" % omega_lambda, ha='right', va='bottom', weight='roman', size='large')
 ax.set_xlabel("$t - t_0$ (Gyr)", fontsize=16)
 ax.set_ylabel("$a$", fontsize=16)
-plt.show()
-#fig.savefig("test.pdf", bbox_inches="tight")
+fig.savefig("plots/expansion.png", bbox_inches="tight", transparent=True)
+# plt.show()  # Show the plot interactively if you want
+# For reports, save a pdf plot. For quick viewing, save a png
+# fig.savefig("plots/expansion.pdf", bbox_inches="tight", transparent=True)
--- fit_models.py
+++ fit_models.py
@@ -1,72 +1,74 @@
+import os
 import numpy as np
 from scipy.integrate import quad
+from chainconsumer import ChainConsumer
 
 
-# ----------Define the function we want to integrate----------
-def Hz_inverse(z, OM, OL):
-    Hz = np.sqrt((1 + z) ** 2 * (OM * z + 1) - OL * z * (z + 2));
-    return 1.0 / Hz;
+def Hz_inverse(z, om, ol):
+    """ Calculate 1/H(z). Will integrate this function. """
+    Hz = np.sqrt((1 + z) ** 2 * (om * z + 1) - ol * z * (z + 2))
+    return 1.0 / Hz
 
 
-# ----------Function to calculate the distance modulus--------
-# ----------correcting for curvature of the universe----------
 def dist_mod(zs, om, ol):
+    """ Calculate the distance modulus, correcting for curvature"""
     ok = 1.0 - om - ol
     x = np.array([quad(Hz_inverse, 0, z, args=(om, ol))[0] for z in zs])
     if ok < 0.0:
         R0 = 1 / np.sqrt(-ok)
         D = R0 * np.sin(x / R0)
-
-    if ok > 0.0:
+    elif ok > 0.0:
         R0 = 1 / np.sqrt(ok)
         D = R0 * np.sinh(x / R0)
-
-    if ok == 0.0:
+    else:
         D = x
-
-    LumDist = D * (1 + zs)
-    DM = 5 * np.log10(LumDist)
-    return DM
+    lum_dist = D * (1 + zs)
+    dist_mod = 5 * np.log10(lum_dist)
+    return dist_mod
 
 
 # ---------- Import data -------------------------------------
-data = np.loadtxt("data.txt")
-zz = data[:, 0]
-mu = data[:, 2]
-mu_error = data[:, 3] + 0.02
+data = np.genfromtxt("data/data.txt", names=True)
+zz = data["Z"]
+mu = data["MU"]
+mu_error = data["MUERR"] + 0.02
 mu_error2 = mu_error ** 2
 # Define cosntants
 H0 = 70.0
 c_H0 = 3.e5 / H0
 
 # ---------- Set up fitting ranges ---------------------------
-n = 6  # Increase this for a finer grid
+n = 10  # Increase this for a finer grid
 oms = np.linspace(0, 0.7, n)
 ols = np.linspace(0, 1.0, n)
 n_marg = 200
 mscr_guess = 5.0 * np.log10(c_H0) + 25
 mscr = np.linspace(mscr_guess - 0.5, mscr_guess + 0.5, n_marg)
 
-chi2 = np.ones((n, n)) * 9e9  # Our chi2 values, set initially to super large values
+chi2 = np.ones((n, n)) * np.inf  # Our chi2 values, set initially to super large values
 mscr_used = np.zeros((n, n))
 
 # ---------- Do the fit ---------------------------
-for i, om in enumerate(oms):
-    for j, ol in enumerate(ols):
-        mu_model = dist_mod(zz, om, ol)
-        for k, m in enumerate(mscr):
-            mu_model_norm = mu_model + m;
-            chi2_test = np.sum((mu_model_norm - mu) ** 2 / mu_error2);
-            if chi2_test < chi2[i, j]:
-                chi2[i, j] = chi2_test
-                mscr_used[i, j] = k
-    print("Done %d out of %d" % (i, oms.size))
+saved_data_filename = "data/saved_grid_%d.txt" % n
 
-np.savetxt("myfit.txt", chi2, fmt="%10.4f")  # Back this up or something yo
+if os.path.exists(saved_data_filename):  # Load the last run with n grid if we can find it
+    print("Loading saved data. Delete %s to regenerate the points\n" % saved_data_filename)
+    chi2 = np.loadtxt(saved_data_filename)
+else:
+    for i, om in enumerate(oms):
+        for j, ol in enumerate(ols):
+            mu_model = dist_mod(zz, om, ol)
+            for k, m in enumerate(mscr):
+                mu_model_norm = mu_model + m
+                chi2_test = np.sum((mu_model_norm - mu) ** 2 / mu_error2)
+                if chi2_test < chi2[i, j]:
+                    chi2[i, j] = chi2_test
+                    mscr_used[i, j] = k
+        print("Done %d out of %d" % (i, oms.size))
+    np.savetxt(saved_data_filename, chi2, fmt="%10.4f")
+
 likelihood = np.exp(-0.5 * chi2)
-
-from chainconsumer import ChainConsumer
-
 c = ChainConsumer()
-c.add_chain([oms, ols], weights=likelihood, grid=True, parameters=[r"$\Omega_m$", r"$\Omega_\Lambda$"])
-c.plot(filename="output.png", figsize="page")
+c.add_chain([oms, ols], weights=likelihood, grid=True, parameters=[r"$\Omega_m$", r"$\Omega_\Lambda$"], name="Supernova")
+c.plotter.plot(filename="plots/fit.png", figsize="column")
+print(c.analysis.get_latex_table())
